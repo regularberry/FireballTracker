03ad26a80af9b762db2c864e96025f1e044069c8
Sean Berry <sean@regularberry.com>
2017-03-03 12:58:37 -0600

    integration tests for ApiClient and Parser

--- FireballTrackerTests/FireballApiClientTests.swift
+++ FireballTrackerTests/FireballApiClientTests.swift
@@ -28,12 +28,12 @@ class FireballApiClientTests: XCTestCase {
         // WHEN: we ask the server for the latest fireballs
         let latestCompleted = expectation(description: "LatestFireballs")
         client.getLatestFireballs(completion: {(fireballs, error) in
+            // THEN: completion gets called with no error
             if error == nil {
                 latestCompleted.fulfill()
             }
         })
         
-        // THEN: completion gets called with no error
         waitForExpectations(timeout: 10, handler:nil)
     }
     
@@ -44,12 +44,72 @@ class FireballApiClientTests: XCTestCase {
         // WHEN: we ask the server for fireballs before now
         let beforeCompleted = expectation(description: "FireballAfterDate")
         client.getFireballs(beforeDate: Date(), completion: {(fireballs, error) in
+            // THEN: completion gets called with no error
             if error == nil {
                 beforeCompleted.fulfill()
             }
         })
         
-        // THEN: completion gets called with no error
+        waitForExpectations(timeout: 10, handler:nil)
+    }
+    
+    func test_thatApiClient_grabsFireballCountFromChunkSize() {
+        // GIVEN: an api client, parser and chunk size
+        let chunkSize = 10
+        let client = FireballApiClient(parser: FireballParser(), chunkSize: chunkSize)
+        
+        // WHEN: we ask the server for the latest fireballs
+        let latestCompleted = expectation(description: "LatestFireballs")
+        client.getFireballs(beforeDate: Date(), completion: {(fireballs, error) in
+            guard error == nil else {
+                return
+            }
+            // THEN: fireball count is the same as chunk size
+            XCTAssert(fireballs.count == chunkSize)
+            latestCompleted.fulfill()
+        })
+        
+        
+        waitForExpectations(timeout: 10, handler:nil)
+    }
+    
+    func test_thatApiClient_grabsFireballCountFromChunkSizeBeforeDate() {
+        // GIVEN: an api client, parser and chunk size and a date
+        let chunkSize = 30
+        let date = Date()
+        let client = FireballApiClient(parser: FireballParser(), chunkSize: chunkSize)
+        
+        // WHEN: we ask the server for the fireballs before date
+        let beforeCompleted = expectation(description: "FireballsBefore")
+        client.getFireballs(beforeDate: date, completion: {(fireballs, error) in
+            guard error == nil else {
+                return
+            }
+            // THEN: fireball count is the same as chunk size
+            XCTAssert(fireballs.count == chunkSize)
+            beforeCompleted.fulfill()
+        })
+        
+        waitForExpectations(timeout: 10, handler:nil)
+    }
+    
+    func test_thatApiClient_grabsNothingWhenDateTooOld() {
+        // GIVEN: an api client, parser and chunk size and a date
+        let chunkSize = 100
+        let date = Date(timeIntervalSince1970: 0)
+        let client = FireballApiClient(parser: FireballParser(), chunkSize: chunkSize)
+        
+        // WHEN: we ask the server for the fireballs before date
+        let beforeCompleted = expectation(description: "FireballsBefore")
+        client.getFireballs(beforeDate: date, completion: {(fireballs, error) in
+            guard error == nil else {
+                return
+            }
+            // THEN: fireball count is 0
+            XCTAssert(fireballs.count == 0)
+            beforeCompleted.fulfill()
+        })
+        
         waitForExpectations(timeout: 10, handler:nil)
     }
 }
