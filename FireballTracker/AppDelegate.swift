030f76c18497f35a9d3843e82df94526e45fcf53
Sean Berry <sean@regularberry.com>
2017-03-05 18:12:46 -0600

    Set Launch screen color Using custom fonts on navigation bar

--- FireballTracker/AppDelegate.swift
+++ FireballTracker/AppDelegate.swift
@@ -15,7 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        setCustomFonts()
+        
         return true
+    }
+    
+    func setCustomFonts() {
+        let fontName = "Futura"
+        if let font = UIFont(name: fontName, size: 20),
+            let smallerFont = UIFont(name: fontName, size: 16) {
+            UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: font]
+            UIBarButtonItem.appearance().setTitleTextAttributes([NSFontAttributeName: smallerFont], for: .normal)
+        }
     }
 
 }
